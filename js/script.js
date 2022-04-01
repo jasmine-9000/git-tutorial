6165b1bf7d5850f0b318584ae98ea6b25f010d9a
Jasmine <wrkerr9@gmail.com>
2022-04-01 13:23:21 -0700

    Added more photos and added javascript to display those photos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+window.onload = () => {
+    const heroImage = document.getElementById("hero__image");
+    const hoverChangeElementArray = [...document.querySelectorAll(".changeImageOnHover")];
+    hoverChangeElementArray.forEach((element,index) => {
+        element.addEventListener("mouseover", (e) => {
+            const newSource = e.target.dataset.src;
+            heroImage.src = "/img/" + newSource + ".jpg";
+            console.log("Hello")
+        })
+    })
+}
